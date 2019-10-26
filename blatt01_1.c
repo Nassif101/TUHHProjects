8320152a8948155ba8cf2dbd2bd59c45735aac2b
Nassif101 <57049131+Nassif101@users.noreply.github.com>
2019-10-27 00:24:09 +0200

    First Commit

---
+++ blatt01_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>	/*Präprozessor direktive*/
+
+int main() {	/*Main-Funktion soll zuerst aufgerufen werden*/
+  int a, b;		/*der Datentyp bestimmen*/
+
+  printf("\n\n Hallo TUTOR! \n\n");	/*einfaches Text-Output*/
+  a = 50;	/*Zuweisung*/
+  b = 20;
+  printf("Die Antwort ist: \t %d \n\n", a + b); /*Text-Output mit einer Operation*/
+
+  return 0;	
+}

fe80699bf0c9649f64683d75a40e1ada5387bbba
heisenberg101101 <57050815+heisenberg101101@users.noreply.github.com>
2019-10-27 19:13:24 +0100

    lol

--- Makefile
+++ Makefile
@@ -32,5 +32,5 @@
 #
 
 all:
-	gcc -std=c99 -pedantic -Wall -Wextra -o blatt01_3.exe blatt01_3.c
-	blatt01_3
+	gcc -std=c99 -pedantic -Wall -Wextra -o blatt01_1.exe blatt01_1.c
+	blatt01_1
--- blatt01_1.c
+++ blatt01_1.c
@@ -4,9 +4,9 @@ int main() {	/*Main-Funktion soll zuerst aufgerufen werden*/
   int a, b;		/*der Datentyp bestimmen*/
 
   printf("\n\n Hallo TUTOR! \n\n");	/*einfaches Text-Output*/
-  a = 50;	/*Zuweisung*/
+  a = 22;	/*Zuweisung*/
   b = 20;
   printf("Die Antwort ist: \t %d \n\n", a + b); /*Text-Output mit einer Operation*/
 
-  return 0;	/*0 bedeutet kein Fehler*/
+  return 0;	
 }
